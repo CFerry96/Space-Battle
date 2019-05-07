144dd8e0a2b8ac074ba9ce355b7e6aba48fbe5b6
CFerry96 <c16349346@mydit.ie>
2019-05-07 03:23:15 +0100

    Final Push

---
+++ Space Battle/Assets/CameraController.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CameraController : MonoBehaviour
+{
+
+    public GameObject Cam1, Cam2, Cam3, Cam4, Cam5, boom, song;
+    public Image Black;
+    bool end = false;
+    //public AudioSource song;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("transitions");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(end == true)
+        {
+            var tempVariable = Black.color;
+            tempVariable.a += Time.deltaTime / 2.5f;
+            Black.color = tempVariable;
+        }
+    }
+
+    IEnumerator transitions()
+    {
+        yield return new WaitForSeconds(17);
+
+        Cam1.SetActive(false);
+        Cam2.SetActive(true);
+
+        yield return new WaitForSeconds(10);
+
+        Cam2.SetActive(false);
+        Cam3.SetActive(true);
+
+        yield return new WaitForSeconds(6);
+
+        Cam3.SetActive(false);
+        Cam4.SetActive(true);
+
+        yield return new WaitForSeconds(6);
+
+        Cam4.SetActive(false);
+        Cam1.SetActive(true);
+
+        yield return new WaitForSeconds(6);
+
+        Cam1.SetActive(false);
+        Cam5.SetActive(true);
+
+        yield return new WaitForSeconds(10);
+        end = true;
+
+        yield return new WaitForSeconds(5);
+        song.SetActive(false);
+        boom.SetActive(true);
+        
+        //Cam5.SetActive(false);
+    }
+}
+
+
+
+
---
+++ Space Battle/Assets/Shoot.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+
+    private bool cooldown;
+    public GameObject bullet;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (cooldown == false)
+        {
+            StartCoroutine("fire");
+        }
+    }
+
+    void Bullet()
+    {
+        Instantiate(bullet, this.transform.position, Quaternion.identity);
+
+    }
+
+    IEnumerator fire()
+    {
+        cooldown = true;
+        Bullet();
+        yield return new WaitForSeconds(4);
+       
+        cooldown = false;
+    }
+}
